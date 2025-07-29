38cb245379be4cec78395da3fcb815d877918b13
muhammadyunus12 <abdullohrustamov9605@gmail.com>
2025-07-29 18:39:18 +0500

    first commit

---
+++ scirpt.js
@@ -0,0 +1,42 @@
+
+const hafta =  ;
+
+
+switch(true){
+   case hafta: 1;
+     console.log("Dushanba");
+    break;
+    
+    case hafta: 2;
+    console.log("seshanba");
+    break;
+
+    case hafta: 3;
+    console.log("chorshanba");
+    break;
+
+
+    case hafta : 4;
+    console.log("payshanba");
+    break;
+
+
+    case hafta : 5;
+    console.log("juma");
+    break;
+
+
+    case hafta : 6;
+    console.log("shanba");
+    break;
+
+
+    case hafta : 7;
+    console.log("yakshanba");
+    break;
+
+    default :
+    console.log("bunaqa hafta yo'q");
+     
+      
+}
