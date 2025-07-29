dbf41aa3287861767d5493b7d14290093f406b96
muhammadyunus12 <abdullohrustamov9605@gmail.com>
2025-07-29 20:33:43 +0500

    first commit

--- scirpt.js
+++ scirpt.js
@@ -1,42 +1,147 @@
 
-const hafta =  ;
+
+// misol n1
 
 
-switch(true){
-   case hafta: 1;
-     console.log("Dushanba");
-    break;
+
+// let hafta = 1;
+
+
+// switch(hafta){
+//    case 1:
+//      console.log("Dushanba");
+//     break;
     
-    case hafta: 2;
-    console.log("seshanba");
-    break;
+//     case  2:
+//     console.log("seshanba");
+//     break;
 
-    case hafta: 3;
-    console.log("chorshanba");
-    break;
-
-
-    case hafta : 4;
-    console.log("payshanba");
-    break;
+//     case 3: 
+//     console.log("chorshanba");
+//     break;
 
 
-    case hafta : 5;
-    console.log("juma");
-    break;
+//     case 4 : 
+//     console.log("payshanba");
+//     break;
 
 
-    case hafta : 6;
-    console.log("shanba");
-    break;
+//     case 5: 
+//     console.log("juma");
+//     break;
 
 
-    case hafta : 7;
-    console.log("yakshanba");
-    break;
+//     case 6:
+//     console.log("shanba");
+//     break;
 
-    default :
-    console.log("bunaqa hafta yo'q");
+
+//     case  7:
+//     console.log("yakshanba");
+//     break;
+
+//     default :
+//     console.log("bunaqa hafta yo'q");
      
       
-}
+// }
+
+
+
+// misol N2
+
+
+//  const oy =  13;
+
+//  switch(oy){
+//   case 12 :
+//   case 1 :
+//   case 2 :
+//     console.log("qish");
+//     break;
+    
+//   case 3 :
+//   case 4 :
+//   case 5 :
+//     console.log("bahor");
+//     break;
+    
+//   case 6 :
+//   case 7 :
+//   case 8 :
+//     console.log("yoz");
+//     break;
+    
+//   case 9 :
+//   case 10 :
+//   case 11 :
+//     console.log("kuz");
+//     break;
+    
+//     default :
+//     console.log("bunaq oy yo'z");
+   
+
+
+//     misol n3
+
+
+
+
+//  const son =   ; 
+ 
+ 
+//  switch(son){
+//   case 123:
+//     console.log("true");
+//     break;
+
+//     case 144:
+//       console.log("false");
+//       break;
+//  }
+
+
+// misol n4
+// const number = Number(prompt("Son kiriting"));
+
+// if (number > 0) {
+//   let a = number + 1;
+//   console.log(a);
+// } else {
+//   let b = number - 2;
+//   console.log(b);
+// }
+
+//  number1 = Number(prompt("birinchi sonni kriting"));
+//  number2 = Number(prompt("ikkinchi sonni kriting"));
+//  number3 = Number(prompt("uchinchi sonni kriting"));
+//  musbatlarSoni = 0;
+//  manfiylarSoni = 0;
+
+// if (number1 > 0) {
+//   musbatlarSoni =1;
+// } else {
+//   manfiylarSoni = manfiylarSoni + 1;
+// }
+
+// if (number2 > 0) {
+//   musbatlarSoni = musbatlarSoni + 1;
+// } else {
+//   manfiylarSoni = manfiylarSoni + 1;
+// }
+
+// if (number3 > 0) {
+//   musbatlarSoni = musbatlarSoni + 1;
+// } else {
+//   manfiylarSoni = manfiylarSoni + 1;
+// }
+
+// console.log(`manfiylar soni ${manfiylarSoni}`);
+// console.log(`musbatlar soni ${musbatlarSoni}`);
+
+// misol n5
+
+
+
+      
